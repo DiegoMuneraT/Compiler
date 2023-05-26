fc27e5bea9d0aa2aa4d17f805816e649a228a9d0
Castro-1 <jecastrog@eafit.edu.co>
2023-05-26 16:27:21 -0500

    tabla mejorada pero no corregida

--- Proyecto/BottomUp/Table.py
+++ Proyecto/BottomUp/Table.py
@@ -7,33 +7,30 @@ class Table:
 
     def create_table(self):
         tabla = {}
-        productions = {}
-        for left, right in self.grammar.rules.items():
-            A = right[0]
-            productions[left] = A
+        productions = self.grammar.rules
+        first = self.grammar.first
+        Nterminals = self.grammar.Nterminals
         # print("Productions\n")
         # print(productions)
-        # print("First\n")
-        # print(self.grammar.first,'\n','\n')
-        # print("Follow\n")
-        # print(self.grammar.follow,'\n','\n')
+        print(self.grammar.rules)
         
-        for A in productions:
-            tabla[A] = {}
-            for a in self.grammar.first[A]:
+        for nterminal in productions:
+            tabla[nterminal] = {}
+            for a in first[nterminal]:
+                print(a)
 
-                for b in productions[A]:
+                for b in productions[nterminal]:
                     if a in b:
-                        tabla[A][a] = b
+                        tabla[nterminal][a] = b
                 
-                if b[0] in self.grammar.Nterminals:
-                    if a in self.grammar.first[b[0]]:
-                        tabla[A][a] = b
+                if b[0] in Nterminals:
+                    if a in first[b[0]]:
+                        tabla[nterminal][a] = b
 
 
-            if 'epsilon' in self.grammar.first[A]:
-                for b in self.grammar.follow[A]:
-                    tabla[A][b] = productions[A]
+            if 'epsilon' in first[nterminal]:
+                for b in self.grammar.follow[nterminal]:
+                    tabla[nterminal][b] = productions[nterminal]
 
         self.table = tabla
     
